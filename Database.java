18c0dfd381de7d58fd96768c4cf9c56ff74426e0
anammunshi <anammunshi@gmail.com>
2016-10-28 12:48:22 +0530

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,85 @@
+package com.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper
+{
+
+	private static final int DATABASE_VERSION=1;
+
+	private static final String DATABASE_NAME="JSON_Sample";
+
+	private static final String TABLE_CONTACTS="json_contact";
+
+	private static final String CONTACT_ID = "CONTACT_ID";
+	private static final String FIRST_NAME = "FIRST_NAME";
+	private static final String LAST_NAME = "LAST_NAME";
+	private static final String AGE = "AGE";
+	private static final String ADDRESS = "ADDRESS";
+	private static final String CITY = "CITY";
+	private static final String STATE = "STATE";
+	private static final String POSTALCODE = "POSTALCODE";
+	private static final String TYPE = "TYPE";
+	private static final String NUMBER = "NUMBER";
+	
+
+	public Database(Context context) 
+	{
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db)              
+	{
+		db.execSQL("drop table if exists "+TABLE_CONTACTS);
+		// TODO Auto-generated method stub
+		String sql = "CREATE TABLE " + TABLE_CONTACTS + "(" +CONTACT_ID  + " TEXT,"+ FIRST_NAME +  " TEXT," + LAST_NAME + " TEXT," + AGE + " TEXT,"
+		+ADDRESS  +" TEXT," + CITY + " TEXT," + STATE + " TEXT," + POSTALCODE + " TEXT," + TYPE + " TEXT," + NUMBER + " TEXT" +")";
+		db.execSQL(sql);
+	}       
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) 
+	{
+		// TODO Auto-generated method stub
+		db.execSQL("drop table if exists "+TABLE_CONTACTS);
+		onCreate(db);
+
+	}
+
+
+	public void addContacts(String contact_id,String firstname, String lastname, String age, String streetadd,String city,String state,String postalcode,String type,String number)
+	{
+		SQLiteDatabase db= this.getWritableDatabase();
+		
+		ContentValues  values= new ContentValues();
+		values.put(CONTACT_ID,contact_id);
+		values.put(FIRST_NAME,firstname);
+		values.put(LAST_NAME,lastname);
+		values.put(AGE,age);
+		values.put(ADDRESS,streetadd);
+		values.put(CITY,city);
+		values.put(STATE,state);
+		values.put(POSTALCODE,postalcode);                            
+		values.put(TYPE, type);
+		values.put(NUMBER,number);
+		
+		db.insert(TABLE_CONTACTS, null, values);
+
+	}
+	
+	public Cursor getContacts()
+	{
+		SQLiteDatabase db= this.getWritableDatabase();
+		
+		String sql = "SELECT rowid _id,* FROM "+TABLE_CONTACTS;
+		Cursor cursor=db.rawQuery(sql, null);
+		return cursor;
+	}
+
+}
